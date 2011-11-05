d3ea68891242dc3e44926d620d315eb3df83ef4c
kerryck <kerryck@lappy386.(none)>
2011-11-05 15:42:32 -0700

    Remove mootools dependancy.

--- resources/js/links.js
+++ resources/js/links.js
@@ -1,12 +1,16 @@
 (function(){
 /* Removes all the mark elements from the page. */
 var clearHighlights = function(){
-    var marks = document.getElements('mark');
+    var marks = document.querySelectorAll('mark');
+    marks = Array.prototype.slice.call(marks);
+
     marks.forEach(function(mark){
         var parent = mark.parentNode;
-        
-        while (mark.childNodes.length) parent.insertBefore(mark.firstChild, mark);
-        parent.removeChild(mark);    
+
+        while (mark.childNodes.length) {
+            parent.insertBefore(mark.firstChild, mark);
+        }
+        parent.removeChild(mark);
     });
 };
 
@@ -20,39 +24,42 @@ var highlight = function(listToHighlight, event){
         event.cancelBubble = true;
     }
     clearHighlights();
+
     listToHighlight.forEach(function(text){
-        var treeWalker = document.createTreeWalker($('content'), 
+        var treeWalker = document.createTreeWalker(document.querySelector('#content'),
             NodeFilter.SHOW_TEXT,
             { acceptNode: function(node) { return NodeFilter.FILTER_ACCEPT; } },
 	        false
 	    );
 
         var node, textContent;
-        while( node = treeWalker.nextNode() ){
+        while( (node = treeWalker.nextNode()) ){
             textContent = node.textContent;
-            if(node.textContent.contains(text)){
+            if(node.textContent.indexOf(text) !== -1){
                 node = node.splitText(textContent.indexOf(text));
                 node.splitText(text.length);
-                
-                var mark = new Element('mark').wraps(node);
+
+                var mark = document.createElement('mark');
+                mark.innerHTML = node.textContent;
+                node.parentElement.replaceChild(mark, node);
                 setTimeout(function(){
-                    mark.setStyle('background', 'rgba(175, 214, 175,0.7)');
+                    mark.style['background-color'] = 'rgba(175, 214, 175,0.7)';
                 }, 1);
                 break;
             }
         }
     });
 };
-    
+
 document.addEventListener('DOMContentLoaded', function(){
-    document.getElement('*[href$=do.html]').addEventListener('click',
+    document.querySelector('[href$="do.html"]').addEventListener('click',
         highlight.bind(null, ['software engineering', 'be devoted', 'work @ Inkling']), true);
-    document.getElement('*[href$=make.html]').addEventListener('click',
+    document.querySelector('[href$="make.html"]').addEventListener('click',
         highlight.bind(null, ['web wizardy', 'This website', 'projects']), true);
-    document.getElement('*[href$=contact.html]').addEventListener('click',
+    document.querySelector('[href$="contact.html"]').addEventListener('click',
         highlight.bind(null, ['me@kerryck.com']), true);
 }, true);
 
-})()
+})();
 
 
